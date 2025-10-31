7fda9a1a344b5b80a74c38a09684a3e281ee43f1
VahagMardyan <mardyan2003@gmail.com>
2025-10-31 19:05:07 +0400

    Updated

--- test.cpp
+++ test.cpp
@@ -3,9 +3,9 @@
 #include <cmath>
 #include <limits>
 
-template<class T>
-void print(T value) {
-    std::cout<<value<<std::endl;
+template<class ...Args>
+void print(Args&&... args) {
+    ((std::cout << args << " "), ...) << std::endl;
 }
 
 int global;
@@ -16,7 +16,7 @@ int main() {
     const int c = a + b;
     constexpr int d = 5;
     constexpr int e = d + 1;
-    print(e);
+    // print(e);
     enum EyeColor {
         Brown, // 0 
         Blue, // 1
@@ -36,9 +36,42 @@ int main() {
         x+=1.0;
     }
     int local; // 
+    // char *str = "Hello"; // impossible
+    const char* str = "Hello"; // possible
+    print(&str);
     print(std::exp(709)); // 8.21841e+307
     print(std::exp(710)); // inf
     print(global); // 0
     print(local); // 32766, 32767, ... -> garbage
+    int q = 2.5;
+    print(q); // 2
+    // int k {2.45};
+    // print(k); // error: narrowing conversion ...
+    
+    /* Casts */
+    // 1. Static cast
+    int r = 10;
+    double t = static_cast<double>(r); // int -> double
+    print(sizeof(t));
+
+    // 2. Const cast
+    const int l = 10;
+    int *p = const_cast<int*>(&l);
+    *p = 20; // undefined behavior
+    *p += 15;
+
+    print(*p); // expected 35
+
+    // 3. Reinterpret cast
+    int m = 65;
+    char *j = reinterpret_cast<char*>(&m);
+    print(*j); // A
+
+    // 4. C-style cast
+    double h = (double)m;
+    print(sizeof(h), h); // 8 65
+
+    // // print function testing
+    print(5, 6, "abs");
     return 0;
 }
